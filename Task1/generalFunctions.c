1863e383c9317c9e8f0fefc3374de7bc44ef06fd
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2021-09-10 23:52:38 +0200

    Create GPIO drivers

---
+++ Task1/generalFunctions.c
@@ -0,0 +1,17 @@
+/*
+ * generalFunctions.c
+ *
+ *  Created on: Sep 8, 2021
+ *      Author: AhmedAlaa
+ */
+
+#include "generalFunctions.h"
+
+void _delay_ms(uint32 time)
+{
+    uint32 periodic_time = (uint32)((1 / F_CPU) * 1000000000); // The time of one clock by nanosecond
+    uint32 num_of_clocks = (uint32)(time * 1000000 / periodic_time);
+    for(;num_of_clocks > 0; num_of_clocks--);
+}
+
+
---
+++ Task1/stdtypes.h
@@ -0,0 +1,24 @@
+/*
+ * stdtypes.h
+ *
+ *  Created on: Sep 7, 2021
+ *      Author: AhmedAlaa
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long  int uint32;
+
+typedef signed char sint8;
+typedef signed short int sint16;
+typedef signed long  int sint32;
+
+typedef float  f32;
+typedef double f64;
+
+
+#endif /* STDTYPES_H_ */
