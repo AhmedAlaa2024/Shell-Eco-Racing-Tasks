fe51f80f55211272c7384474ca4d3241b0dba60d
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2021-09-30 14:18:42 +0200

    Fix an error

--- Task3_Example2/vtasks.c
+++ Task3_Example2/vtasks.c
@@ -20,28 +20,24 @@ void vTask1_Comm(void* pvParameter)
 {
     xQueue1_colors = xQueueCreate(2, sizeof(uint8_t));
     xQueue2_colors_names = xQueueCreate(2, sizeof(char*));
+
     uint8_t i = 0, led_color = RED, flag_low = 1; // Working on falling edge
     char* current_color = "RED";
-    uint32_t data;
+    volatile uint32_t data;
     while(1)
     {
         data = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
-        if((data == 0) && (flag_low == 1))
+
+        if ((data == 0) && (flag_low == 1))
         {
-            vTaskDelay(30);
+            led_color = colors[i];
+            current_color = color_names[i];
+            ++i;
 
-            data = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
-            if((data == 0) && (flag_low == 1))
-            {
-                led_color = colors[i];
-                current_color = color_names[i];
-                ++i;
+            xQueueSend(xQueue1_colors, (void*)&led_color, (TickType_t)1000);
+            xQueueSend(xQueue2_colors_names, (void*)&current_color, (TickType_t)1000);
 
-                xQueueSend(xQueue1_colors, (void*)&led_color, (TickType_t)1000);
-                xQueueSend(xQueue2_colors_names, (void*)current_color, (TickType_t)1000);
-
-                i = i % 7;
-            }
+            i = i % 7;
         }
         flag_low = data;
         vTaskDelay(100);
@@ -55,15 +51,15 @@ void vTask2_action(void* pvParameter)
     char* current_color = "RED";
     while(1)
     {
-        xQueueReceive(xQueue1_colors, &led_color, (TickType_t)0);
-        if (xQueueReceive(xQueue2_colors_names, current_color, (TickType_t)0))
+        xQueueReceive(xQueue1_colors, &(led_color), (TickType_t)0);
+        if (xQueueReceive(xQueue2_colors_names, &(current_color), (TickType_t)0))
         {
             send_str(current_color);
             send_newline();
         }
         GPIOPinWrite(GPIO_PORTF_BASE, led_color, led_color);
         vTaskDelay(50);
-        GPIOPinWrite(GPIO_PORTF_BASE, led_color, 0);
+        GPIOPinWrite(GPIO_PORTF_BASE, led_color, 0x00);
         vTaskDelay(50);
     }
 }
